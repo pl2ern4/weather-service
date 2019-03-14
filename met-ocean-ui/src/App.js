a6deeee0aa511a7ef95cf2b3f42f16cdeb2488c0
Prerna <chaturvediprerna03@gmail.com>
2019-03-14 12:04:26 +0530

    seperatecomponents

--- met-ocean-ui/src/App.js
+++ met-ocean-ui/src/App.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import TableComponent from './TableComponent';
 import './App.css';
 
--- met-ocean-ui/src/TableComponent.js
+++ met-ocean-ui/src/TableComponent.js
@@ -2,106 +2,15 @@ import React,{Component} from 'react';
 import PropTypes from 'prop-types';
 import { withStyles } from '@material-ui/core/styles';
 import Table from '@material-ui/core/Table';
-import TableHead from '@material-ui/core/TableHead'; 
-import TableSortLabel from '@material-ui/core/TableSortLabel'; 
-import Tooltip from '@material-ui/core/Tooltip';
+import TableHead from '@material-ui/core/TableHead';
 import TableBody from '@material-ui/core/TableBody';
 import TableCell from '@material-ui/core/TableCell';
 import TableFooter from '@material-ui/core/TableFooter';
 import TablePagination from '@material-ui/core/TablePagination';
 import TableRow from '@material-ui/core/TableRow';
 import Paper from '@material-ui/core/Paper';
-import IconButton from '@material-ui/core/IconButton';
-import FirstPageIcon from '@material-ui/icons/FirstPage';
-import KeyboardArrowLeft from '@material-ui/icons/KeyboardArrowLeft';
-import KeyboardArrowRight from '@material-ui/icons/KeyboardArrowRight';
-import LastPageIcon from '@material-ui/icons/LastPage';
+import TablePaginationActionsWrapped from './TablePaginationActions'
 import moment from 'moment';
-
-const actionsStyles = theme => ({
-  root: {
-    flexShrink: 0,
-    color: theme.palette.text.secondary,
-    marginLeft: theme.spacing.unit * 2.5,
-  },
-});
-
-class TablePaginationActions extends Component {
-  handleFirstPageButtonClick = event => {
-    this.props.onChangePage(event, 0);
-  };
-
-  handleBackButtonClick = event => {
-    this.props.onChangePage(event, this.props.page - 1);
-  };
-
-  handleNextButtonClick = event => {
-    this.props.onChangePage(event, this.props.page + 1);
-  };
-
-  handleLastPageButtonClick = event => {
-    this.props.onChangePage(
-      event,
-      Math.max(0, Math.ceil(this.props.count / this.props.rowsPerPage) - 1),
-    );
-  };
-
-  render() {
-    const { classes, count, page, rowsPerPage, theme } = this.props;
-
-    return (
-      <div className={classes.root}>
-        <IconButton
-          onClick={this.handleFirstPageButtonClick}
-          disabled={page === 0}
-          aria-label="First Page"
-        >
-          {theme.direction === 'rtl' ? <LastPageIcon /> : <FirstPageIcon />}
-        </IconButton>
-        <IconButton
-          onClick={this.handleBackButtonClick}
-          disabled={page === 0}
-          aria-label="Previous Page"
-        >
-          {theme.direction === 'rtl' ? <KeyboardArrowRight /> : <KeyboardArrowLeft />}
-        </IconButton>
-        <IconButton
-          onClick={this.handleNextButtonClick}
-          disabled={page >= Math.ceil(count / rowsPerPage) - 1}
-          aria-label="Next Page"
-        >
-          {theme.direction === 'rtl' ? <KeyboardArrowLeft /> : <KeyboardArrowRight />}
-        </IconButton>
-        <IconButton
-          onClick={this.handleLastPageButtonClick}
-          disabled={page >= Math.ceil(count / rowsPerPage) - 1}
-          aria-label="Last Page"
-        >
-          {theme.direction === 'rtl' ? <FirstPageIcon /> : <LastPageIcon />}
-        </IconButton>
-      </div>
-    );
-  }
-}
-
-TablePaginationActions.propTypes = {
-  classes: PropTypes.object.isRequired,
-  count: PropTypes.number.isRequired,
-  onChangePage: PropTypes.func.isRequired,
-  page: PropTypes.number.isRequired,
-  rowsPerPage: PropTypes.number.isRequired,
-  theme: PropTypes.object.isRequired,
-};
-
-const TablePaginationActionsWrapped = withStyles(actionsStyles, { withTheme: true })(
-  TablePaginationActions,
-);
-
-let counter = 0;
-function createData(name, calories, fat) {
-  counter += 1;
-  return { id: counter, name, calories, fat };
-}
 
 const styles = theme => ({
   root: {
@@ -138,7 +47,7 @@ class TableComponent extends Component {
 
   componentWillReceiveProps(nextProps){
     const data = [];
-    Object.keys(nextProps.data).map((obj)=>{data.push(nextProps.data[obj])});
+    Object.keys(nextProps.data).forEach((obj)=>{data.push(nextProps.data[obj])});
     this.setState({data});
     
   }
@@ -154,7 +63,7 @@ class TableComponent extends Component {
           <TableHead>
             <TableRow>
             {rows.map(
-                row => (
+                row => 
                 <TableCell
                     key={row.id}
                     align={'left'}
@@ -162,7 +71,7 @@ class TableComponent extends Component {
                 >
                     {row.label}
                 </TableCell>
-                ),
+                ,
                 this,
             )}
             </TableRow>
@@ -173,18 +82,18 @@ class TableComponent extends Component {
                     return;
                 }
                 return (
-                  <TableRow key={n.id}>
+                  <TableRow key={'row_'+i}>
                   
-                    {i%8==0 && <TableCell rowSpan="8">
+                    {i%8===0 && <TableCell key={'cell_date_'+i} rowSpan="8">
                       {moment(n.datetime).utc().format('YYYY-MM-DD')}
                     </TableCell>}
-                    <TableCell>
+                    <TableCell key={'cell_time_'+i}>
                       {moment(n.datetime).utc().format('HH:mm:ss')}
                     </TableCell>
-                    <TableCell>{n.sea_surface_wave_significant_height!=="null" &&`${n.sea_surface_wave_significant_height}m`||'--'}</TableCell>
-                    <TableCell>{`${n.air_temperature_at_2m_above_ground_level}`||'--'}</TableCell>
-                    <TableCell>{`${n.wind_from_direction_at_10m_above_ground_level}\xB0`||'--'}</TableCell>
-                    <TableCell>{`${n.wind_speed_at_10m_above_ground_level}m/s`||'--'}</TableCell>
+                    <TableCell key={'cell_wave_'+i}>{n.sea_surface_wave_significant_height!=="null" &&`${n.sea_surface_wave_significant_height}m`||'--'}</TableCell>
+                    <TableCell key={'cell_temp_'+i}>{`${n.air_temperature_at_2m_above_ground_level}`||'--'}</TableCell>
+                    <TableCell key={'cell_directon_'+i}>{`${n.wind_from_direction_at_10m_above_ground_level}\xB0`||'--'}</TableCell>
+                    <TableCell key={'cell_speed_'+i}>{`${n.wind_speed_at_10m_above_ground_level}m/s`||'--'}</TableCell>
                   </TableRow>
                 );
               })}
